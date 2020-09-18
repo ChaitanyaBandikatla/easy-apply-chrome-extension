e9374dddc74cfad8ce68fa4f1e5a3a61f12913c2
Chaitanya Bandikatla <chaitanyab19952007@gmail.com>
2020-09-18 14:19:13 -0400

    Changing cors

--- Application/easyapply/src/main/java/com/example/easyapply/EasyapplyApplication.java
+++ Application/easyapply/src/main/java/com/example/easyapply/EasyapplyApplication.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class EasyapplyApplication {
@@ -26,7 +28,7 @@ public class EasyapplyApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOrigins("*");
 			}
 		};
 	}
