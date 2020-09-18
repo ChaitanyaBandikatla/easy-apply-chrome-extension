4fac489558bf0a7fb71b57fd2261f1c9b5a584d1
Chaitanya Bandikatla <chaitanyab19952007@gmail.com>
2020-09-18 15:38:49 -0400

    Changing cors

--- Application/easyapply/src/main/java/com/example/easyapply/EasyapplyApplication.java
+++ Application/easyapply/src/main/java/com/example/easyapply/EasyapplyApplication.java
@@ -28,7 +28,7 @@ public class EasyapplyApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOrigins("*");
+				registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH").allowedOrigins("*").allowedHeaders("Content-type");
 			}
 		};
 	}
