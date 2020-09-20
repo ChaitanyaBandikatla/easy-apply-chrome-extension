6fd87bc0a8a17d0a56c54966ced7815189fe035d
santoshpavan <santoshpavan.psp@gmail.com>
2020-09-20 18:58:18 -0400

    default to login page on startup

--- frontend/easyapply/src/app/app.component.ts
+++ frontend/easyapply/src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'easyapply';
-  registrationForm: FormGroup;
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.navigateByUrl('login');
+  }
 }
